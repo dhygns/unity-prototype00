0ab32f35cb36e98dde29774608bdcee86051fca0
Donghyeon Kim <dhygns@naver.com>
2017-07-27 02:37:06 +0900

    init

---
+++ Assets/Scripts/ObjectPicker.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPicker : MonoBehaviour {
+    
+    private ObjectUIBehavior _ui_script;
+
+    private delegate void UpdateList();
+    private UpdateList _update_statement;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this._event_processor();
+
+        if(this._update_statement != null) this._update_statement();
+    }
+
+
+    private void _event_processor()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit _hit = new RaycastHit();
+            Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(_ray, out _hit, 100.0f))
+                this._grab_setup(_hit.collider);
+        }
+        
+        if(Input.GetMouseButtonUp(0))
+        {
+            this._release_setup();
+        }
+    }
+
+    private void _grab_setup(Collider other)
+    {   
+        
+
+        if (this._update_statement != this._grab_update)
+        {
+            this._update_statement = new UpdateList(this._grab_update);
+
+            this._ui_script = other.GetComponent<ObjectUIBehavior>() as ObjectUIBehavior;
+
+            this._ui_script.Picked();
+        }
+        else return;
+    }
+
+    private void _grab_update()
+    {
+
+    }
+
+    private void _release_setup()
+    {
+
+        if (this._update_statement != this._release_update)
+        {
+            this._update_statement = new UpdateList(this._release_update);
+
+            //Throwing or Releasing is determined here.
+            //this._ui_script.Released();
+            this._ui_script.Throwed();
+            this._ui_script = null;
+        }
+        else return;
+    }
+
+    private void _release_update()
+    {
+
+    }
+
+    
+}
---
+++ Assets/Scripts/ObjectUIBehavior.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectUIBehavior : MonoBehaviour
+{
+
+
+    private delegate void UpdateList();
+    private UpdateList _updatelists;
+
+    private Transform _ts_pivot;
+    private Transform _ts_child;
+
+    private Material _mt_child;
+    private Color _cl_chlid;
+
+    private Vector3 _rotation_velocity;
+    private Vector3 _rotation;
+
+    private Vector3 _accelate;
+    private Vector3 _velocity;
+    private Vector3 _position;
+
+    // Use this for initialization
+    void Start()
+    {
+        this._ts_child = this.transform;
+        this._ts_pivot = this.transform.parent.GetComponentInParent<Transform>();
+
+        this._mt_child = this.GetComponent<Renderer>().material;
+        this._cl_chlid = Color.white;
+
+        this._position.x = Random.Range(-5.0f, 5.0f);
+        this._position.y = Random.Range(1.0f, 2.0f);
+        this._position.z = Random.Range(-1.0f, -2.0f);
+
+        this._ts_pivot.localPosition = this._position;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this._updatelists != null) this._updatelists();
+
+
+        this._mt_child.color = this._cl_chlid;
+
+        this._ts_pivot.localPosition = this._position;
+        this._ts_child.eulerAngles = this._rotation;
+    }
+
+
+    public void Picked()
+    {
+        //when is it picked, it will do once
+        this._updatelists = new UpdateList(this._pickedUpdate);
+
+        this._rotation_velocity.x = 0.0f; //Random.Range(-1.0f, 1.0f) * 180.0f;
+        this._rotation_velocity.y = Random.Range(-0.2f, 0.2f) * 180.0f;
+        this._rotation_velocity.z = Random.Range(-0.2f, 0.2f) * 180.0f;
+    }
+
+    private void _pickedUpdate()
+    {
+        this._rotation += this._rotation_velocity * Time.deltaTime;
+        this._rotation.x = (this._rotation.x + 360.0f) % 360.0f;
+        this._rotation.y = (this._rotation.y + 360.0f) % 360.0f;
+        this._rotation.z = (this._rotation.z + 360.0f) % 360.0f;
+
+
+        //Mouse Mapping With Camera
+        Vector3 prev = this._position;
+
+        Vector3 mpos = Input.mousePosition;
+
+        mpos.z = this._position.z - Camera.main.transform.localPosition.z;
+
+        this._position = Camera.main.ScreenToWorldPoint(mpos);//this._velocity * Time.deltaTime;
+        this._position.z = prev.z;
+
+        this._rotation += -this._rotation * Time.deltaTime;
+
+        this._accelate = (this._position - prev) * 20.0f;
+        this._accelate.z = Random.Range(20.0f, 30.0f);
+    }
+
+
+
+    public void Released()
+    {
+        //when is it Released, it will do once
+        this._updatelists = new UpdateList(this._releasedUpdate);
+
+        this._cl_chlid.a = 0.0f;
+        this._position.x = Random.Range(-5.0f, 5.0f);
+        this._position.y = Random.Range(1.0f, 2.0f);
+        this._position.z = Random.Range(-1.0f, -2.0f);
+    }
+
+    private void _releasedUpdate()
+    {
+        this._cl_chlid.a += (1.0f - this._cl_chlid.a) * Time.deltaTime;
+
+        this._rotation.x = (this._rotation.x) % 360.0f;
+        this._rotation.y = (this._rotation.y) % 360.0f;
+        this._rotation.z = (this._rotation.z) % 360.0f;
+
+        this._velocity += -this._velocity * Time.deltaTime * 10.0f;
+
+        this._position += this._velocity * Time.deltaTime;
+        this._rotation += -this._rotation * Time.deltaTime;
+    }
+
+    public void Throwed()
+    {
+        //when is it Released, it will do once
+        this._updatelists = new UpdateList(this._throwedUpdate);
+        
+        this._velocity = this._accelate;
+
+
+        this._rotation_velocity.x = 0.0f; //Random.Range(-1.0f, 1.0f) * 180.0f;
+        this._rotation_velocity.y = Random.Range(-1.0f, 1.0f) * 180.0f;
+        this._rotation_velocity.z = Random.Range(-1.0f, 1.0f) * 180.0f;
+
+    }
+
+    private void _throwedUpdate()
+    {
+        this._velocity += - 0.5f * (this._velocity.magnitude * this._velocity.magnitude) * this._velocity.normalized * Time.deltaTime;
+
+
+        this._position += this._velocity * Time.deltaTime;
+        this._rotation += -this._rotation * Time.deltaTime;
+        
+
+        if (Vector3.Distance(Camera.main.transform.localPosition, this._position) > 20.0)
+        {
+            this.Released();
+        }
+    }
+
+}
