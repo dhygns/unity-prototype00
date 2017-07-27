9a0d6180e6d271d751ffb50827bebf8d0fd93c41
Donghyeon Kim <dhygns@naver.com>
2017-07-27 23:16:23 +0900

    make object return

--- Assets/Scripts/ObjectUIBehavior.cs
+++ Assets/Scripts/ObjectUIBehavior.cs
@@ -61,6 +61,7 @@ public class ObjectUIBehavior : MonoBehaviour
         this._rotation_velocity.x = 0.0f; //Random.Range(-1.0f, 1.0f) * 180.0f;
         this._rotation_velocity.y = Random.Range(-0.2f, 0.2f) * 180.0f;
         this._rotation_velocity.z = Random.Range(-0.2f, 0.2f) * 180.0f;
+        this._accelate = Vector3.zero;
     }
 
     private void _pickedUpdate()
@@ -83,35 +84,45 @@ public class ObjectUIBehavior : MonoBehaviour
 
         this._rotation += -this._rotation * Time.deltaTime;
 
-        this._accelate = (this._position - prev) * 20.0f;
-        this._accelate.z = Random.Range(20.0f, 30.0f);
+        Vector3 oldacc = this._accelate * (1.0f - Time.deltaTime);
+        Vector3 newacc = (this._position - prev);
+        
+        this._accelate = this._accelate * 0.9f + newacc * 6.0f;
+        this._accelate.z = Mathf.Sign(this._accelate.y) * Random.Range(10.0f, 20.0f);
     }
 
 
 
-    public void Released()
+    public void Turnback()
     {
         //when is it Released, it will do once
-        this._updatelists = new UpdateList(this._releasedUpdate);
+        this._updatelists = new UpdateList(this._turnbackUpdate);
 
-        this._cl_chlid.a = 0.0f;
-        this._position.x = Random.Range(-5.0f, 5.0f);
-        this._position.y = Random.Range(1.0f, 2.0f);
-        this._position.z = Random.Range(-1.0f, -2.0f);
+        //this._cl_chlid.a = 0.0f;
+        //this._position.x = Random.Range(-5.0f, 5.0f);
+        //this._position.y = Random.Range(1.0f, 2.0f);
+        //this._position.z = Random.Range(-1.0f, -2.0f);
     }
 
-    private void _releasedUpdate()
+    private void _turnbackUpdate()
     {
-        this._cl_chlid.a += (1.0f - this._cl_chlid.a) * Time.deltaTime;
+        //this._cl_chlid.a += (1.0f - this._cl_chlid.a) * Time.deltaTime;
 
-        this._rotation.x = (this._rotation.x) % 360.0f;
-        this._rotation.y = (this._rotation.y) % 360.0f;
-        this._rotation.z = (this._rotation.z) % 360.0f;
+        //this._rotation.x = (this._rotation.x) % 360.0f;
+        //this._rotation.y = (this._rotation.y) % 360.0f;
+        //this._rotation.z = (this._rotation.z) % 360.0f;
 
-        this._velocity += -this._velocity * Time.deltaTime * 10.0f;
+        if (this._position.magnitude > 10.0)
+        {
+            this._velocity += -2.0f * this._position.normalized * Time.deltaTime * 10.0f;
+        }
+        else
+        {
+            this._velocity += -0.5f * (this._velocity.magnitude * this._velocity.magnitude) * this._velocity.normalized * Time.deltaTime;
+        }
 
         this._position += this._velocity * Time.deltaTime;
-        this._rotation += -this._rotation * Time.deltaTime;
+        this._rotation += -this._rotation_velocity * Time.deltaTime;
     }
 
     public void Throwed()
@@ -136,11 +147,15 @@ public class ObjectUIBehavior : MonoBehaviour
         this._position += this._velocity * Time.deltaTime;
         this._rotation += -this._rotation * Time.deltaTime;
         
-
-        if (Vector3.Distance(Camera.main.transform.localPosition, this._position) > 20.0)
+        if (this._position.magnitude > 10.0)
         {
-            this.Released();
+            this.Turnback();
         }
     }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null) this._velocity.y *= -1.0f;
+    }
 }
